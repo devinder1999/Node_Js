be8a56dff4a67766473363806d13c86dc797e3cf
Davinder Singh <147801575+devinder1999@users.noreply.github.com>
2025-01-25 19:19:29 +0530

    practice

---
+++ if-else.js
@@ -0,0 +1,12 @@
+exports.program =function(a,b)
+{
+
+    if(a>b)
+    {
+        console.log("Number is Small")
+    }
+    else{
+        console.log("Number is Big")
+    }
+    return "output a gya"
+}
---
+++ practice.js
@@ -0,0 +1,4 @@
+var project=require("./if-else")
+ var outout=project.program(5,15)
+
+ console.log(output)
